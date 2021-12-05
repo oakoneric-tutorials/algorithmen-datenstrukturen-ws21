de8ffb40372a7d42a94731ea191049532a15981c
eric <k-eric@outlook.com>
2021-12-05 21:31:33 +0100

    Update tut09-2.c
    Erklärung zum Einbinden, Copyright Joshua, eine kurze Variante fürs Maximum und eine passende main

--- tut09/tut09-2.c
+++ tut09/tut09-2.c
@@ -11,6 +11,11 @@ Aufgabe 2
 #include <stdio.h>
 #include <stdlib.h>
 #include "print_tree.h"
+/*
+In print_tree.h bzw. print_tree.c wird eine Ausgabe für Bäume definiert (c) Joshua Uhlig.
+Die dafür notwendige Datenstruktur wurde mit ausgelagert und ist daher hier auskommentiert.
+Compile mit "gcc print_tree.c tut09-2.c -o tut09-2"
+*/
 
 /* 
 typedef struct node *tree;
@@ -27,11 +32,11 @@ int height(tree t)
 {
     if (t == NULL)
         return 0;
+
     int hl = height(t->left);
-    int hr = heigth(t->right);
-    if (hl > hr)
-        return hl + 1;
-    return hr + 1;
+    int hr = height(t->right);
+
+    return (hl > hr) ? hl + 1 : hr + 1;
 }
 
 tree bfs(tree t)
@@ -45,15 +50,17 @@ tree bfs(tree t)
     return bf;
 }
 
-void lRot(tree *root)
+// Teil (b)
+
+void lRot(tree *tp)
 {
-    if (root == NULL || *root == NULL)
+    if (tp == NULL || *tp == NULL)
         return;
 
-    tree rightChild = (*root)->right;
-    (*root)->right = rightChild->left;
-    rightChild->left = *root;
-    *root = rightChild;
+    tree rightChild = (*tp)->right;
+    (*tp)->right = rightChild->left;
+    rightChild->left = *tp;
+    *tp = rightChild;
 }
 
 /* ----------------------------------------- */
@@ -69,34 +76,43 @@ tree createNode(int n, tree l, tree r)
 
 int main()
 {
-    // zu Teil (a)
     printf("\n============ Teil (a) ========================\n");
-    tree bsp =
-        createNode(4,
-                   createNode(5, NULL, NULL),
+    tree t =
+        createNode(1,
                    createNode(2,
-                              createNode(0, NULL, NULL),
-                              NULL));
-    printf("bsp = \n");
-    print_t(bsp);
-
-    tree t1 = createNode(8,
-                         createNode(5,
-                                    createNode(3, createNode(2, NULL, NULL), createNode(4, NULL, NULL)),
-                                    createNode(7, createNode(6, NULL, NULL), NULL)),
-                         createNode(10,
-                                    createNode(9, NULL, NULL),
-                                    createNode(11, NULL, NULL)));
-    printf("t1 = \n");
-    print_t(t1);
-
-    tree s = createNode(2, createNode(3, createNode(2, NULL, NULL), createNode(4, NULL, NULL)), createNode(1, NULL, NULL));
-    printf("s = \n");
-    print_t(s);
-
-    tree t = createNode(2, createNode(2, NULL, NULL), createNode(3, NULL, NULL));
+                              NULL,
+                              createNode(4, NULL, NULL)),
+                   createNode(3, NULL, NULL));
     printf("t = \n");
     print_t(t);
+
+    tree s = bfs(t);
+    printf("s = bfs(t) = \n");
+    print_t(s);
+
+    printf("\n============ Teil (b) ========================\n");
+    // Beispiel aus Aufgabe 1, erster Baum
+    tree t2 = createNode(2,
+                         createNode(1, NULL, NULL),
+                         createNode(3,
+                                    NULL,
+                                    createNode(4,
+                                               NULL,
+                                               createNode(5, NULL, NULL))));
+    printf("t2 = \n");
+    print_t(t2);
+
+    printf("bfs(t2) = \n");
+    print_t(bfs(t2));
+
+    printf("lRot(t2->right) = \n");
+    lRot(&(t2->right));
+
+    printf("t2 = \n");
+    print_t(t2);
+
+    printf("bfs(t2) = \n");
+    print_t(bfs(t2));
 
     return 0;
 }
